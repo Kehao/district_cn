d179587b56cc93ca757b00b9be11488748860c5d
kehao <kehao.qiu@gmail.com>
2013-08-24 04:59:54 +0800

    add coverralls

--- Gemfile
+++ Gemfile
@@ -6,3 +6,5 @@ gem "rake"
 gem 'rspec', :require => false
 gem 'rails', '~> 3.2.0', :require => false
 gem 'sqlite3', :platform => [:ruby, :mswin, :mingw]
+
+gem 'coveralls', require: false
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require 'active_record'
 require File.expand_path("../../lib/district_cn",  __FILE__)
 require 'rspec/autorun'
 #require 'active_support/core_ext/kernel/reporting'
-require 'logger'
-require 'pp'
+
+require 'coveralls'
+Coveralls.wear!
 
